49fdb25a4aab203d2b856916cf591f1d37121ef6
David H <iq.davidh@gmail.com>
2019-07-14 15:30:38 -0500

    ya tenemos static

--- front/serverStatic.js
+++ front/serverStatic.js
@@ -10,5 +10,5 @@ app.get("/debug", function (req, res) {
   res.send('Im static');
 });
 
-const port = 3002;
+const port = 4002;
 app.listen(port, () => console.log(`static server running on port ${port}!`));
